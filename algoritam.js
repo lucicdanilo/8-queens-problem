86956c46feea3b3091b8abe420e3d5112cfaa531
Danilo Lučić <lucic.danilo94@gmail.com>
2020-03-26 17:44:04 +0100

    secound commit

--- algoritam.js
+++ algoritam.js
@@ -1,144 +1,128 @@
-// Count the number or row collisions
-var rowCollisions = function(a) {
-  console.log(a);
-  collision = 0;
-  for (var i in a) {
-    for (var j in a) {
-      if (j != i) {
-        collision = a[i] == a[j] ? collision + 1 : collision;
-      }
-    }
+var generisanjeStanja = function() {
+  stanje = [];
+  for (var i = 0; i < 8; i++) {
+    stanje[i] = Math.floor(Math.random() * 8 + 1);
   }
-  return collision;
+  return stanje;
 };
 
-// Broj pravolinijskih napada (provjeriti)
-var pravolinijskiNapadi = function(niz) {
-  // naći bolje ime za od niz-a
-  brojNapada = 0;
-  for (var i in niz) {
-    for (var j in niz) {
-      if ((j = !i)) {
-        brojNapada = niz[i] == niz[j] ? brojNapada++ : brojNapada;
-      }
-    }
+var restartovanjePlaninarenja = function(stanje) {
+  brojac = 1;
+  stanje = planinarenje(stanje);
+  brisanjeKraljica();
+
+  while (!daLiJeRjesenje(stanje)) {
+    stanje = generisanjeStanja(stanje.length);
+    brojac++;
+    stanje = planinarenje(stanje);
   }
+
+  console.log("Broj restartovanja: " + brojac);
+  console.log("Rjesenje: " + stanje);
+  console.log("----------------------------------------");
+
+  document.getElementById("rezultatPlaninarenja").innerHTML =
+    "Broj restartovanja: " + brojac + "<br> Rjesenje je: " + stanje;
+
+  crtanjeKraljica(stanje);
+  return [brojac, stanje];
 };
 
-// Count the number of column collisions
-var diaCollisions = function(a) {
-  collision = 0;
-  for (var i in a) {
-    for (var j in a) {
-      if (i != j) {
-        dp = Math.abs(i - j);
-        collision = a[i] == a[j] + dp ? collision + 1 : collision;
-        collision = a[i] == a[j] - dp ? collision + 1 : collision;
-      }
-    }
-  }
-  return collision / 2;
-};
-
-// Broj dijagonalnih napada
-var dijagonalniNapadi = function(niz) {
-  brojNapada = 0;
-  for (var i in niz) {
-    for (var j in niz) {
-      if (i != j) {
-        dp = Math.abs(i - j); // Naci bolji naziv od dp !!!!!
-        brojNapada = a[i] == a[j] + dp ? brojNapada++ : brojNapada;
-        brojNapada = a[i] == a[j] - dp ? brojNapada++ : brojNapada;
-      }
-    }
-  }
-  return brojNapada / 2;
-};
-
-// Heuristic Evaluation Function for N Queens
-// We will want to minimize collisions -- also referred to as min-conflicts in general
-var evaluate = function(s) {
-  return diaCollisions(s) + rowCollisions(s);
-};
-
-// Ocjena polja tj. koliki je broj napada u tom polju!!
-
-var ocjena = function(niz) {
-  return pravolinijskiNapadi(niz) + dijagonalniNapadi(niz);
-};
-
-// Generate a set of candidates.
-var generateCandidates = function(current) {
-  candidates = [];
-  for (var i = 0; i < current.length; i++) {
-    var start = current.slice(0, i);
-    var end = current.slice(i + 1, current.length);
-    for (j = 1; j <= current.length; j++) {
-      var c = start.concat(
-        [Math.floor(Math.random() * current.length + 1)].concat(end)
-      );
-      candidates.push(c);
-    }
-  }
-  return candidates;
-};
-
-// Generate a random new state for the N Queens problem of size n
-var generateState = function(n) {
-  state = [];
-  for (var i = 0; i < n; i++) {
-    state[i] = Math.floor(Math.random() * n + 1);
-  }
-  return state;
-};
-
-// Helper Function to tell us if our configuration is a solution.
-var isSolution = function(config) {
-  return evaluate(config) === 0;
-};
-
-// Da li smo nasli rjesenje
-var daLiJeRjesenje = function(niz) {
-  return ocjeni(niz) === 0;
-};
-
-// Workhorse function that solves our puzzle.
-var nQueensBestFirstHillClimbing = function(start) {
-  var best = start;
-  var current;
-  var currentEval = evaluate(start);
+var planinarenje = function(start) {
+  var najbolji = start;
+  var trenutni;
+  var trenutniOcjena = ocjeni(start);
 
   while (true) {
-    current = best;
-    var candidates = generateCandidates(current);
-    for (var i in candidates) {
-      var candidateEval = evaluate(candidates[i]);
-      // Lower evaluation number is better for our implementation
-      if (candidateEval < currentEval) {
-        current = candidates[i];
-        currentEval = candidateEval;
+    trenutni = najbolji;
+    var kandidati = generisanjeKandidata(trenutni);
+    for (var i in kandidati) {
+      var ocjenaKandidata = ocjeni(kandidati[i]);
+      if (ocjenaKandidata < trenutniOcjena) {
+        trenutni = kandidati[i];
+        trenutniOcjena = ocjenaKandidata;
       }
     }
 
-    // If current & best are STILL the same, then we reached a peak.
-    if (best == current) return best;
+    if (najbolji == trenutni) return najbolji;
 
-    best = current;
+    najbolji = trenutni;
   }
 };
 
-// Solve the N Queens problem using Random Restart Hill Climbing.
-var solveNQueens = function(state) {
-  count = 1;
-  state = nQueensBestFirstHillClimbing(state);
-
-  while (!isSolution(state)) {
-    // Random Restart If not a Solution.
-    state = generateState(state.length);
-    count++;
-    state = nQueensBestFirstHillClimbing(state);
+var generisanjeKandidata = function(stanje) {
+  kandidati = [];
+  for (var i = 0; i < stanje.length; i++) {
+    var start = stanje.slice(0, i);
+    var kraj = stanje.slice(i + 1, stanje.length);
+    for (j = 1; j <= stanje.length; j++) {
+      var c = start.concat(
+        [Math.floor(Math.random() * stanje.length + 1)].concat(kraj)
+      );
+      kandidati.push(c);
+    }
   }
+  return kandidati;
+};
 
-  // Return the Number of Hill Climbing Random Restarts & the SOlution.
-  return [count, state];
+var ocjeni = function(stanje) {
+  var pravolinijskiNapadi = function(stanje) {
+    brojNapada = 0;
+    for (var i in stanje) {
+      for (var j in stanje) {
+        if (j != i) {
+          brojNapada = stanje[i] == stanje[j] ? brojNapada + 1 : brojNapada;
+        }
+      }
+    }
+    return brojNapada;
+  };
+
+  var dijagonalniNapadi = function(stanje) {
+    brojNapada = 0;
+    for (var i in stanje) {
+      for (var j in stanje) {
+        if (i != j) {
+          d = Math.abs(i - j);
+          brojNapada = stanje[i] == stanje[j] + d ? brojNapada + 1 : brojNapada;
+          brojNapada = stanje[i] == stanje[j] - d ? brojNapada + 1 : brojNapada;
+        }
+      }
+    }
+    return brojNapada / 2;
+  };
+
+  return pravolinijskiNapadi(stanje) + dijagonalniNapadi(stanje);
+};
+
+var daLiJeRjesenje = function(stanje) {
+  return ocjeni(stanje) === 0;
+};
+
+var statistika = function() {
+  var brojRestartovanja = 0;
+  for (i = 0; i < 200; i++) {
+    brojRestartovanja =
+      brojRestartovanja + restartovanjePlaninarenja(generisanjeStanja())[0];
+    console.log("Ukupan broj restartovanja: " + brojRestartovanja);
+  }
+  var statistika = brojRestartovanja / 200;
+  console.log("Statistika za 200 realizacija je: " + statistika);
+  document.getElementById("rezultatStatistike").innerHTML =
+    "Statistika za 200 realizacija je: " + statistika;
+  return statistika;
+};
+
+var crtanjeKraljica = function(stanje) {
+  for (var i = 0; i <= 7; i++) {
+    document.getElementById(stanje[i]).children[i].innerHTML = "&#9819;";
+  }
+};
+
+var brisanjeKraljica = function() {
+  for (var i = 1; i <= 8; i++) {
+    for (var j = 0; j <= 7; j++) {
+      document.getElementById(i).children[j].innerHTML = "";
+    }
+  }
 };
